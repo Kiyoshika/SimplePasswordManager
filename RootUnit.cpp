16120126f3acccc609f1059ccda4e145ad2cdca2
Zach Weaver <49159969+Kiyoshika@users.noreply.github.com>
2023-05-29 22:39:19 -0400

    add new categories
    need to fix my local git, so terribly just uploading all source files again

--- RootUnit.cpp
+++ RootUnit.cpp
@@ -5,6 +5,8 @@
 #pragma hdrstop
 
 #include "RootUnit.h"
+#include "NewCategoryDialog.h"
+#include "CategoryExistsDialog.h"
 
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
@@ -14,6 +16,38 @@ TRootForm *RootForm;
 __fastcall TRootForm::TRootForm(TComponent* Owner)
 	: TForm(Owner)
 {
+}
+//---------------------------------------------------------------------------
+
+
+void __fastcall TRootForm::CreateNewCategory(TObject *Sender)
+{
+    NewCategoryForm->Show();
+}
+
+bool __fastcall TRootForm::CheckCategoryNameExists(const String& CategoryName)
+{
+	std::vector<String>::iterator it
+		= std::find(this->CategoryNames.begin(), this->CategoryNames.end(), CategoryName);
+
+	return it != this->CategoryNames.end();
+}
+
+void __fastcall TRootForm::AddNewCategory(const String& NewCategoryName)
+{
+	if (this->CheckCategoryNameExists(NewCategoryName))
+	{
+		// Don't hide the category input, just display error dialog so user
+		// can re-type a new category name
+		CategoryExistsForm->Show();
+		return;
+	}
+		
+	TTreeViewItem* NewCategoryItem = new TTreeViewItem(NULL);
+	NewCategoryItem->Text = NewCategoryName;
+	this->TreeCategories->AddObject(NewCategoryItem);
+	this->CategoryNames.push_back(NewCategoryName);
+	NewCategoryForm->CloseAndClearForm();
 }
 //---------------------------------------------------------------------------
 
